4e5e33a029794b280cfcb3ad6ca5f267beda39c1
MeghanaBobbili <meghanareddybobbili@gmail.com>
2022-11-11 11:46:41 -0600

    update and get single movie by id

--- controllers/movie.js
+++ controllers/movie.js
@@ -57,7 +57,37 @@ exports.movie_delete = function(req, res) {
     res.send('NOT IMPLEMENTED: Movie delete DELETE ' + req.params.id); 
 }; 
  
-// Handle Movie update form on PUT. 
-exports.movie_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Movie update PUT' + req.params.id); 
-}; 
+
+
+// for a specific Costume.
+exports.movie_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await movie.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+   };
+
+   // Handle Costume update form on PUT.
+exports.movie_update_put = async function(req, res) {
+ console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+ try {
+ let toUpdate = await movie.findById( req.params.id)
+ // Do updates of properties
+ if(req.body.movie_Name)
+ toUpdate.movie_Name = req.body.movie_Name;
+ if(req.body.movie_Genre) toUpdate.movie_Genre = req.body.movie_Genre;
+ if(req.body.Rating) toUpdate.Rating= req.body.Rating;
+ let result = await toUpdate.save();
+ console.log("Success " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+ }
+};
