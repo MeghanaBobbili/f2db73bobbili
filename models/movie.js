14570f4af7202dc9f192a89b09a475938b6b709c
MeghanaBobbili <meghanareddybobbili@gmail.com>
2022-11-30 19:46:44 -0600

    final commit

--- models/movie.js
+++ models/movie.js
@@ -1,8 +1,20 @@
 const mongoose = require("mongoose") 
 const movieSchema = mongoose.Schema({ 
- movie_Name: String, 
- movie_Genre: String, 
- Rating: Number 
+ movie_Name: {
+    type: String,
+    min: 1,
+    max: 100
+},
+movie_Genre: {
+    type: String,
+    min: 1,
+    max: 100
+},
+Rating: {
+    type: Number,
+    min: 1,
+    max: 100
+}
 }) 
  
 module.exports = mongoose.model("movie", 
--- routes/movie.js
+++ routes/movie.js
@@ -1,5 +1,13 @@
 var express = require('express');
 const movie_controlers= require('../controllers/movie');
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl; 
+    res.redirect("/login"); 
+  } 
 var router = express.Router();
 
 /* GET costumes */ 
@@ -10,10 +18,10 @@ module.exports = router;
 router.get('/detail', movie_controlers.movie_view_one_Page);
 
 /* GET create costume page */
-router.get('/create', movie_controlers.movie_create_Page);
+router.get('/create',secured, movie_controlers.movie_create_Page);
 
 /* GET create update page */
-router.get('/update', movie_controlers.movie_update_Page); 
+router.get('/update',secured, movie_controlers.movie_update_Page); 
 
 /* GET delete costume page */
-router.get('/delete', movie_controlers.movie_delete_Page);
+router.get('/delete',secured, movie_controlers.movie_delete_Page);
